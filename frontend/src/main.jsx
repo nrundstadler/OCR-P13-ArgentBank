3d02a30a25eeae049e643ea0ba5adf8d6a85445e
Nicolas Rundstadler <n.rundstadler@laposte.net>
2025-05-19 18:30:16 +0200

    Fix: adjust CSS import order to prioritize global styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
+import "./assets/styles/index.scss";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import { router } from "./router";
-import "./assets/styles/index.scss";
 import ProfileDataFetcher from "./components/ProfileDataFetcher/ProfileDataFetcher";
 
 /**
